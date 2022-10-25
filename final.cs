61d06538058eac7c6557ef5aba206e649ba676fa
AlinaVorobeva <114346925+AlinaVorobeva@users.noreply.github.com>
2022-10-26 00:36:38 +0300

    file final.cs

---
+++ final.cs
@@ -0,0 +1,35 @@
+﻿// 
+
+
+string[] array ;
+
+Console.WriteLine("Введите массив строк через запятую: ");
+string stroka = Console.ReadLine();
+array = stroka.Split(' ');
+
+string[] array1 = new string[array.Length];
+int count = 0;
+
+void FinalArray(string[] array, string[] array1)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array1[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FinalArray(array, array1);
+PrintArray(array1);
+
